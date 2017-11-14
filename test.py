1674f1778041b09835b317cbf16000b195197e6f
DESKTOP-DJ29T4D\jmh081701 <1024836002@qq.com>
2017-11-15 07:25:48 +0800

    测试版

--- test.py
+++ test.py
@@ -1,13 +1,35 @@
-import socket
+﻿import socket
+import struct
 sock=socket.socket()
 sock.bind(("127.0.0.1",9090))
 sock.listen(100)
+print("bind well")
 while True:
-	client,clientadd=sock.accept()
-	info=client.recv(1024)
-	print(info)
-	client.send(b'\x00\x5a'+info[2:8])
-	info=client.recv(1024)
-	print(info)	
-	client.close()
+    client,clientadd=sock.accept()
+    infos=client.recv(1024)
+    print(infos)
+    port=struct.unpack("!H",infos[2:4])[0]
+    ip=struct.unpack("!I",infos[4:8])[0]
+    dstport=port
+    dstip=socket.inet_ntoa(struct.pack('I',socket.htonl(ip)))
+    print(dstip,dstport)
+    dstsock=socket.socket()
+    dstsock.connect((dstip,dstport))
+    client.send(b'\x00\x5a'+infos[2:8])
+    while True:
+        try:
+
+            info=client.recv(1024)
+            print("recv from ie")
+            print(info)
+            dstsock.send(info)
+            print("send to host.")
+            dstrecv=dstsock.recv(1024)
+            print("recv from host")
+            print(dstrecv)
+            client.send(dstrecv)
+            print("send to ie.")
+        except:
+            break
+    client.close()
 
